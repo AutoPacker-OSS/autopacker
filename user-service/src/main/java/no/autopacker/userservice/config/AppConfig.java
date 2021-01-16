83c0c8bd672e6d4e7c3f74af817ad9e6e8b26dd3
Aron Nicholasson <aronmar@live.no>
2021-01-17 00:31:50 +0100

    Update AppConfig.java

--- user-service/src/main/java/no/autopacker/userservice/config/AppConfig.java
+++ user-service/src/main/java/no/autopacker/userservice/config/AppConfig.java
@@ -23,11 +23,11 @@ public class AppConfig {
     private String apiClientPassword;
 
     public String getKeycloakAuthUrl() {
-        return apiRootUrl + ":9090/auth";
+        return apiRootUrl + ":8443/auth";
     }
 
     public String getApplicationUrl() {
-        return apiRootUrl + ":8081";
+        return apiRootUrl;
     }
 
     public String getWebApplicationUrl() { return apiRootUrl; }
