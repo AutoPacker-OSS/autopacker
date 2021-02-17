da20c9f6798fe509dd93b06628f377664217b070
Aron Nicholasson <aronmar@live.no>
2021-02-17 22:17:51 +0100

    Add storybook

---
+++ web-application/src/components/Modal/GenericModal.stories.js
@@ -0,0 +1,38 @@
+import React from "react";
+import {GenericModal} from "./GenericModal";
+import {Button} from "antd";
+import {useModals} from "../../context/ModalContext";
+
+export default {
+    title: "components/Modals",
+    component: [GenericModal],
+};
+export const Default = () => {
+    const {openModal, closeModal} = useModals();
+
+    const openGenericModal = () => {
+        openModal(GenericModal, {
+            title: "Modal Title",
+            description: "Modal Description",
+            okText: "Yes",
+            ok: openAnotherGenericModal,
+            cancel: closeModal,
+            cancelText: "Close"
+        })
+    }
+
+    const openAnotherGenericModal = () => {
+        openModal(GenericModal, {
+            title: "Another Modal",
+            okButtonProps: {disabled: true},
+            jsx: <p>This one takes in JSX! And has a button props property!</p>
+        })
+    }
+
+    return (
+        <>
+            <Button onClick={openGenericModal}>Open generic modal</Button>
+        </>
+    );
+};
+
