bd956ed7b26fffc89d887c398eb4c575256516c1
Tiramisu <libanbn@stud.ntnu.no>
2021-02-27 20:24:05 +0100

    Add and allow Windows to use fdapi internal Docker services

--- file-delivery-api/src/main/java/no/autopacker/filedeliveryapi/service/DockerService.java
+++ file-delivery-api/src/main/java/no/autopacker/filedeliveryapi/service/DockerService.java
@@ -1,15 +1,17 @@
 package no.autopacker.filedeliveryapi.service;
 
-import no.autopacker.filedeliveryapi.FileDeliveryApiApplication;
 import no.autopacker.filedeliveryapi.config.DockerConfig;
 import no.autopacker.filedeliveryapi.database.DockerfileRepository;
 import no.autopacker.filedeliveryapi.domain.ModuleMeta;
-import no.autopacker.filedeliveryapi.domain.ProjectMeta;
 import no.autopacker.filedeliveryapi.utils.Utils;
+import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.BeanCreationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
 import java.io.IOException;
 
 @Service
@@ -17,49 +19,97 @@ public class DockerService {
 
 	private DockerfileRepository dockerfileRepo;
 	private DockerConfig dockerConfig;
+	private Logger logger;
 
 	@Autowired
 	public DockerService(DockerfileRepository dockerfileRepo,
-				DockerConfig dockerConfig) throws Exception {
+	                            DockerConfig dockerConfig) throws Exception {
 		this.dockerfileRepo = dockerfileRepo;
 		this.dockerConfig = dockerConfig;
-		loginToDocker();
+		this.logger = LoggerFactory.getLogger(this.getClass());
+
+		initialize();
 	}
 
-	private void loginToDocker() throws Exception {
-		if (System.getProperty("os.name").contains("Linux")) {
-			// Login to docker
-			Runtime
-				.getRuntime()
-				.exec(
-					new String[]{"docker", "login", "--username", dockerConfig.getUsername(),
-						"--password", dockerConfig.getToken(), dockerConfig.getRepository()})
-				.waitFor();
 
-			LoggerFactory.getLogger(FileDeliveryApiApplication.class).info("Logged into docker as "
-				+ dockerConfig.getUsername());
+	/**
+	 * Initialize the DockerService instance.
+	 */
+	private void initialize() {
+		if (!this.isDockerInstalled()) {
+			throw new BeanCreationException("Docker is not found! Please have Docker installed and make sure its " +
+					"root folder is added to the user/system PATH variable.");
 		}
+
+		// Get Docker status command of the login attempt
+		int statusCode = this.loginToDocker();
+
+		if (statusCode != 0) {
+			throw new BeanCreationException("Couldn't login to Docker");
+		}
+
+		logger.info("Logged into Docker as " + dockerConfig.getUsername() + "!");
+	}
+
+	/**
+	 * Runs the docker command to check if Docker is installed and available globally.
+	 * @return  {@code true} if Docker is installed, otherwise it returns {@code false}
+	 */
+	private boolean isDockerInstalled() {
+		boolean isInstalled;
+
+		try {
+			Runtime.getRuntime().exec(new String[]{"docker", "--help"});
+			isInstalled = true;
+		} catch (IOException e) {
+			isInstalled = false;
+		}
+
+		return isInstalled;
+	}
+
+	/**
+	 * Login to docker and store the system status code.
+	 *
+	 * @return              0 (OK) if Docker is logged in or another number if login failed
+	 */
+	public int loginToDocker() {
+		int statusCode;
+
+		try {
+			// Login to docker
+			statusCode = Runtime
+					.getRuntime()
+					.exec(
+							new String[]{"docker", "login", "--username", dockerConfig.getUsername(),
+									"--password", dockerConfig.getToken(), dockerConfig.getRepository()})
+					.waitFor();
+		} catch (IOException | InterruptedException e) {
+			statusCode = -1;
+		}
+
+		return statusCode;
 	}
 
 	public void buildDockerImage(ModuleMeta module, String ownerUsername) throws Exception {
-		if (System.getProperty("os.name").contains("Linux")) {
-			String location = module.getLocation();
+		String location = module.getLocation();
 
-			String dockerFileLocation = dockerfileRepo.findByName(module.getConfigType()).getLocation();
+		String dockerFileLocation = dockerfileRepo.findByName(module.getConfigType()).getLocation();
 
-			String moduleImageName = Utils.instance().getModuleImageName(ownerUsername, module.getName());
-			Runtime cmd = Runtime.getRuntime();
+		String moduleImageName = Utils.instance().getModuleImageName(ownerUsername, module.getName());
+		Runtime cmd = Runtime.getRuntime();
 
-			// Copy dockerfile from repository and save it inside project dir with the standard name "Dockerfile"
-			cmd.exec(new String[]{"cp", dockerFileLocation, location.concat("Dockerfile")}).waitFor();
+		File dockerFileSource = new File(dockerFileLocation);
+		File dockerFileDestination = new File(location.concat("Dockerfile"));
 
-			// Build docker image (the repo part format: kullsyre/username-module)
-			cmd.exec(new String[]{"docker", "build", "-t", dockerConfig.getUsername().concat("/").concat(moduleImageName), location}).waitFor();
+		// Copy dockerfile from repository and save it inside project dir with the standard name "Dockerfile"
+		FileUtils.copyFile(dockerFileSource, dockerFileDestination);
 
-			// Push docker image to repo
-			cmd.exec(new String[]{"docker", "push", dockerConfig.getUsername() + "/" + moduleImageName}).waitFor();
-		} else {
-			LoggerFactory.getLogger(FileDeliveryApiApplication.class).error("Docker image creation failed: Can't use docker in Windows.");
-		}
+		// Build docker image (the repo part format: autopacker/username-module)
+		cmd.exec(new String[]{"docker", "build", "-t", dockerConfig.getUsername().concat("/").concat(moduleImageName),
+				location}).waitFor();
+
+		// Push docker image to repo
+		cmd.exec(new String[]{"docker", "push", dockerConfig.getUsername() + "/" + moduleImageName}).waitFor();
 	}
 }
