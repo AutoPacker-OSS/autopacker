a7fa25866097b846a778c76a42dbaccf255c4e30
Tiramisu <libanbn@stud.ntnu.no>
2021-04-02 18:11:27 +0200

    Update server-prep script to use the latest docker-compose version

--- api/src/main/resources/scripts/linux-prep-server.sh
+++ api/src/main/resources/scripts/linux-prep-server.sh
@@ -11,7 +11,7 @@ apt-get update
 apt-get -y install docker-ce docker-ce-cli containerd.io
 
 # docker-compose installation from https://docs.docker.com/compose/install/#linux
-curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.28.6/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
