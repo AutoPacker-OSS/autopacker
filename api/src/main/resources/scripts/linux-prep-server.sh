c84e48abe5ff834483fb96c2cc3df06dc71eb53c
Tiramisu <libanbn@stud.ntnu.no>
2021-04-02 18:09:26 +0200

    Remove firewall handling from 'linux-prep-server.sh'
    Docker can do this itself

--- api/src/main/resources/scripts/linux-prep-server.sh
+++ api/src/main/resources/scripts/linux-prep-server.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
-
-# Inspiration: https://github.com/NTNU-SysDev/sysdev-templates/blob/master/bash-scripts/springboot.sh
-
-# Step 1, open port SSH, and provided port, and enable ufw
-ufw_status=$(ufw status verbose)
-
-ufw allow ssh
-
-# For testing, allow all incoming and outgoing connections
-ufw default allow outgoing
-ufw default allow incoming
-
-ufw --force enable
-
-# Step 2, download docker and docker-compose
-
 # Docker installation from https://docs.docker.com/engine/install/ubuntu/
-# apt-get -y remove docker docker-engine docker.io containerd runc
 
 apt-get update
 
