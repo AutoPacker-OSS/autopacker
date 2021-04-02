d394cadca6f7723991e613ff1e811cb33ba4982b
Tiramisu <libanbn@stud.ntnu.no>
2021-04-02 17:48:28 +0200

    Update line separator of Gateway API to '\r\n'

--- gateway-api/src/main/java/no/autopacker/gatewayapi/config/SpringGatewayConfig.java
+++ gateway-api/src/main/java/no/autopacker/gatewayapi/config/SpringGatewayConfig.java
@@ -1,1 +1,28 @@
-package no.autopacker.gatewayapi.config;import org.springframework.cloud.gateway.route.RouteLocator;import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;import org.springframework.context.annotation.Bean;import org.springframework.context.annotation.Configuration;@Configurationpublic class SpringGatewayConfig {    @Bean    public RouteLocator router(RouteLocatorBuilder builder) {        return builder.routes()                // API endpoint                .route(r -> r.path("/**")                        .and().header("Host", "api.*")                        .uri("http://localhost:8081")                        .id("api"))                // Web application endpoint                .route(r -> r.path("/**")                        .and().header("Host", "^(?!(api)).*")                        .uri("http://localhost:8444")                        .id("web-app"))                .build();    }}
+package no.autopacker.gatewayapi.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringGatewayConfig {
+
+    @Bean
+    public RouteLocator router(RouteLocatorBuilder builder) {
+        return builder.routes()
+                // API endpoint
+                .route(r -> r.path("/**")
+                        .and().header("Host", "api.*")
+                        .uri("http://localhost:8081")
+                        .id("api"))
+
+                // Web application endpoint
+                .route(r -> r.path("/**")
+                        .and().header("Host", "^(?!(api)).*")
+                        .uri("http://localhost:8444")
+                        .id("web-app"))
+
+                .build();
+    }
+}
